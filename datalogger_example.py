43883bd27553738365b7ffa5bee499c8bfe253fc
Jay Bulsara <jaxbulsara@gmail.com>
2017-11-06 23:16:15 -0500

    Wrote datalogger example.
    The files write too slowly and skip many data points ad high sampling rate.

--- datalogger_example.py
+++ datalogger_example.py
@@ -13,29 +13,34 @@ GPIO.setup(PIN_DRDY, GPIO.IN)
 
 # define gain, sampling rate, and scan mode
 gain = ADS1256_GAIN['1']
-samplingRate = ADS1256_DRATE['10']
+samplingRate = ADS1256_DRATE['30000']
 scanMode = ADS1256_SMODE['SINGLE_ENDED']
 
 adcChannels = 8 - 4 * scanMode
 
+# clear data file
+with open('test_data.csv', 'w') as f:
+	f.write('Time (s)')
+	for i in range(adcChannels):
+		f.write(",Channel {}".format(i))
+	f.write('\n')
+
 def interruptInterpreter(ch):
+	sampleTime = time.time() - startTime
 	if ch == PIN_DRDY:
 		# collect data from ads1256
 		pyadda.collectData()
 
 		volts = pyadda.readAllChannelsVolts(adcChannels)
 
-		i = 0
-		for val in volts:
-			print("Channel {:d} - {:.3f}mV".format(i, val * 1000))
-			i += 1
-
-		# reset to first line
-		print("\033[{:d}A".format(len(volts)+1))
+		# TODO: need a faster way to write this data, as it is skipping samples
+		open('test_data.csv', 'a').write(str(sampleTime) + ','.join(map(str, volts)) + '\n')
 
 
 # setup ads1256 chip
 pyadda.startADC(gain, samplingRate, scanMode)
+
+startTime = time.time()
 
 # wait for DRDY low - indicating data is ready
 GPIO.add_event_detect(PIN_DRDY, GPIO.FALLING, callback=interruptInterpreter)
